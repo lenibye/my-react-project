e00c920323f743a093f0c0113372f5bf96ad121e
lenibye <74501587+lenibye@users.noreply.github.com>
2021-06-14 18:53:34 +0200

    Created react app and installed bootstrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>The Weather Project</h1>
+        <footer>
+         <a href="www.github.com"> Open-source code project</a> by Marlene Bay
+        </footer>
+      
+      </div>
+    </div>
+  );
+}
+
